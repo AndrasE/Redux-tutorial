913b4a4291d8a3afb7ae450be17c548d6fd5433b
Andras E <rohadtsajt@gmail.com>
2023-08-27 13:59:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const redux = require("redux")
+const createStore = redux.createStore
+
 const BUY_CAKE = "BUY_CAKE" 
+const BUY_ICECREAM = "BUY ICECREAM"
 
 function buyCake() {
     return {
@@ -8,7 +12,8 @@ function buyCake() {
 }
 
 const initalState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20
 }
 
 const reducer = (state = initalState, action) => {
@@ -17,7 +22,19 @@ const reducer = (state = initalState, action) => {
             ...state,
             numOfCakes: state.numOfCakes - 1
         }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
 
         default: return state
     }
 }
+
+const store =  createStore(reducer)
+console.log("Inital state", store.getState())
+const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
