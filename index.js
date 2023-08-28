ab788fd042c90b319f445a9ff56e5860ddf323ae
Andras E <rohadtsajt@gmail.com>
2023-08-28 16:57:08 +0100

    async

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const reduxLogger = require("redux-logger")
 
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
 const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = "BUY_CAKE" 
@@ -74,9 +75,10 @@ const rootReducer = combineReducers({
     cake: cakeReducer,
     iceCream: iceCreamReducer
 })
-const store =  createStore(rootReducer)
+
+const store =  createStore(rootReducer, applyMiddleware(logger))
 console.log("Inital state", store.getState())
-const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyIceCream())
